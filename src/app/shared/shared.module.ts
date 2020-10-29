16b56e77cd56757b8c6a0ec56cd8623e3443fe0a
Алексей Сергеевич Почтарев <pochtarevAS@parus31.local>
2020-10-29 18:24:57 +0300

    -update modiles -add material theme and components -add logo image -add requests proxy

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {  
+    ReactiveFormsModule,
+    FormsModule
+} from '@angular/forms';
+
+import { MaterialModule } from './material/material.module';
+
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        ReactiveFormsModule,
+        FormsModule,
+        MaterialModule
+    ]
+})
+export class SharedModule { }
